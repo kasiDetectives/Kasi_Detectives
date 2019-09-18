0c16d4df50b1c5b8e2ef7dee397c36229886e722
Willington Mnisi <willington.mnisi@gmail.com>
2019-09-18 16:04:36 +0200

    search function with customised array

--- src/app/community-event/community-event.page.ts
+++ src/app/community-event/community-event.page.ts
@@ -16,6 +16,9 @@ export class CommunityEventPage implements OnInit {
   user
   searchString
   result
+  coordinates = []
+  features =[]
+  place =[]
   constructor(public navigationService : NavigationService, public userService : UsersService, public mapboxService : MapboxService, public router : Router, public events: Events) {
     console.log("why");
     this.checkState()
@@ -36,8 +39,46 @@ export class CommunityEventPage implements OnInit {
     this.mapboxService.autoComplete(this.searchString).subscribe((data) => {
        this.result = data
        console.log(this.result);
-    })
+        this.features = this.result.features
+        console.log(this.features);
+        
+   
+        for(let i = 0; i < this.features.length; i++){
+        this.coordinates.push(this.features[i])
+        this.place.push({
+          coordinates : this.features[i].geometry.coordinates,
+          place : this.features[i].place_name,
+          name : this.features[i].text,
+          // region : this.features[i].context[0].text,
+          // regionCode : this.features[i].context[0].short_code,
+          // country : this.features[i].context[1].text,
+          //code : this.features[i].context[1].wikidata,
+          context : this.features[i].context
+        })
+        }
+      
+      
+        console.log("Hello  ------------------------------------");   
+      console.log(this.features);
+      
+       console.log(this.coordinates);
+       //this.check(this.result)
 
+       console.log("place           --------------------------");
+       console.log(this.place);
+       
+    })
+    
+   
+
+  }
+  check(data){
+    let result = data
+    let coordinates
+    for(let i = 0; i < result.length; i++){
+     coordinates = result.features[i].geometry.coordinates
+    }
+    console.log(coordinates);
   }
   ngOnInit() {
   }
--- src/app/mapbox.service.ts
+++ src/app/mapbox.service.ts
@@ -8,9 +8,12 @@ export class MapboxService {
 
   constructor(public http : HttpClient) { }
   city = "Pretoria"
+  address = '//api.mapbox.com/geocoding/v5/mapbox.places/'
    AppID = "&APPID=13e0cbb2f2cede6f03e901ab6f8f53e8"
+   returnParameters = "limit=10"
+   apiKey = 'pk.eyJ1Ijoid2lsMW5ndDBuIiwiYSI6ImNrMG1mbnZqcDEydjMzbW40bWNtMmU4NjgifQ.6ggdrR-Y2ng55vBDhU7Xlg'
   autoComplete(place){
-    return this.http.get('//api.mapbox.com/geocoding/v5/mapbox.places/' + place + '.json?limit=20&access_token=pk.eyJ1Ijoid2lsMW5ndDBuIiwiYSI6ImNrMG1mbnZqcDEydjMzbW40bWNtMmU4NjgifQ.6ggdrR-Y2ng55vBDhU7Xlg')
+    return this.http.get(this.address + place + '.json?' + this.returnParameters + '&access_token=' + this.apiKey)
  
   }
 }
