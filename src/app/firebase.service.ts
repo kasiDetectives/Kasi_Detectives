096b0c1b80fc3b5661759fab915654b693c4e83c
Willington Mnisi <willington.mnisi@gmail.com>
2019-10-04 13:24:24 +0200

    added fetch functionality for crime types

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase'
+import { ValueAccessor } from '@ionic/angular/dist/directives/control-value-accessors/value-accessor';
 
 
 var database = firebase.database();
@@ -8,18 +9,9 @@ var database = firebase.database();
 })
 export class FirebaseService {
   crimesList : Array<any> = []
+  savedLocations : Array<any> = []
+  tempArray : Array<any> = []
   constructor() { }
-
-  fetchCrimeCategoriess(){
-  var result = firebase.database().ref().child('CrimeTypes')
-  result.on('child_added', snap =>{
-    this.crimesList.push(snap.key)
-    console.log(this.crimesList);
-    
-   })
-
-    return this.crimesList
-  }
 
   fetchCrimeCategories(){
   return firebase.database().ref().child('CrimeTypes').once('value').then(result =>{
@@ -32,5 +24,35 @@ export class FirebaseService {
       console.log(error);
       return error
     })
+  }
+
+  fetchSavedLocations(){
+    firebase.database().ref().child('HighRisk').on('child_added', result=>{
+      let locations = JSON.parse(JSON.stringify(result))
+      console.log(locations);
+     
+      this.clearArray(this.tempArray)
+      for(let key in locations){
+        
+        this.tempArray.push({
+          place : Object.values(locations[key])
+        })
+      
+      }
+      console.log(this.tempArray);
+     for(let i = 0; i < this.tempArray.length; i++){
+      this.savedLocations.push({
+        crimeType: this.tempArray[i].place[0],
+        lat: this.tempArray[i].place[1],
+        lng: this.tempArray[i].place[2]
+      })
+    }
+      console.log(this.savedLocations);
+      
+    })
+    return this.savedLocations
+  }
+  clearArray(array){
+    for(let i=0; i < array.length; i++){array.splice(i)}
   }
 }
