be303ea9d81ffcc93b1100d5eb9dbb58ff7dd9e9
Will1ngt0n <willington.mnisi@gmail.com>
2019-10-23 19:23:39 +0200

    autosaves

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
 import { AlertController, Events } from '@ionic/angular';
 import { Router } from '@angular/router'
-import {FormBuilder, Validators, CheckboxRequiredValidator} from '@angular/forms'
-import { NavigationService } from '../navigation.service';
+import {FormBuilder, Validators} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -15,20 +14,15 @@ export class LoginPage implements OnInit {
   emailPattern : string = "[a-zA-Z0-9-_.+#$!=%^&*/?]+[@][a-zA-Z0-9-]+[.][a-zA-Z0-9]+"
   passwordPattern = "^(?=.*\[0-9])(?=.*\[a-z])(?=.*\[A-Z])(?=.*\[@#$!%^&*,.<>]).{8,}$"
   loginForm
-  pageURL = "und"
   constructor(
     public userService: UsersService,
     public alertController: AlertController,
     public route: Router,
     public formBuilder: FormBuilder,
-    public navigationService : NavigationService,
     public events : Events,
     ) {
       this.events.publish('currentPage:home', false)
-      this.pageURL = this.navigationService.returnPageURL()
-      console.log(this.pageURL);
-    //this.checkURL()
-    this.loginForm = formBuilder.group({
+      this.loginForm = formBuilder.group({
     
       email: [this.email, Validators.compose(
         [Validators.required, Validators.pattern(this.emailPattern)]
@@ -40,13 +34,7 @@ export class LoginPage implements OnInit {
    this.loginForm.get('email').setValue('willington.mnisi@gmail.com')
    this.loginForm.get('password').setValue('Will1ngt0n7&')
   }
-  checkURL(){
-    if(this.pageURL==="und"){
-      this.pageURL = this.navigationService.returnPageURL()
-      console.log(this.pageURL);
-    }
-    
-  }
+  
   login(){
     this.email = this.loginForm.get('email').value
     this.password = this.loginForm.get('password').value
@@ -55,35 +43,21 @@ export class LoginPage implements OnInit {
       if(result.operationType === "signIn"){
         this.events.publish('user:loggedIn', result.user.email);
         console.log("Welcome " + result.user.email)
-        let userId = result.user.uid
-        if(this.pageURL==="report-alert" || this.pageURL==="community-event"){
-          let link = "/" + this.pageURL
-          console.log(link);
-          this.route.navigate([link])
-        }else{
-          let link = "home"
-          this.route.navigate([link])
-        }
+        let link = "home"
+        this.route.navigate([link])
         console.log('why are you running again?');
-      //   if(this.boolean === true){
-      //     console.log(true);
-      //   }
-      // }else{
-      //   console.log(result.message)
-       }else{
+      }else{
         this.invalidPassword(result)
       }
     })
   }
 
-  async invalidPassword(result)
-  {
+  async invalidPassword(result) {
     const alert = await this.alertController.create({
       header: "Alert",
       message: result,
       buttons: ['OK']
     })
-
     await alert.present()
   }
   //Resetting user password using email password reset request
@@ -91,22 +65,20 @@ export class LoginPage implements OnInit {
     const alert = await this.alertController.create({
       header: 'Reset Password',
       message: 'Are ypou sure you want to reset your password?',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          cssClass: 'success',
-          handler: (blah) => {
-            console.log('Confirm Cancel: blah');
-          }
-        }, {
-          text: 'Yes',
-          handler: (user) => {
-            console.log('Confirm Okay');
-            this.userService.passwordReset(user.email)
-          }
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'success',
+        handler: (blah) => {
+          console.log('Confirm Cancel: blah');
         }
-      ]
+      }, {
+        text: 'Yes',
+        handler: (user) => {
+          console.log('Confirm Okay');
+          this.userService.passwordReset(user.email)
+        }
+      }]
     });
     await alert.present();
   }
